ac5f4d2ab5a74e034605dfa8f6e8dc8582be2dd8
Pérez Niño Alvaro <155790737+aperezn298@users.noreply.github.com>
2025-03-06 13:06:15 -0500

    Funciones

---
+++ 04_Ejemplos/FunDeclaradas.js
@@ -0,0 +1,40 @@
+// // Declarada sin parametros y sin retorno
+// function SumarNumeros (){
+//     let numUno = 14;
+//     let numDos = 5;
+//     let resultado = numUno + numDos;
+//     console.log(`El resultado de la suma es: ${resultado}`);
+// }
+// SumarNumeros();
+
+// // // Declarada con parametros y sin retorno
+// function SumarNumeros (uno, dos){
+//     let numUno = uno;
+//     let numDos = dos;
+//     let resultado = uno + dos;
+//     resultado = numUno + numDos;
+//     console.log(`El resultado de la suma es: ${resultado}`);
+// }
+// SumarNumeros(34, 23);
+
+// // Declarada con parametros y retorno
+// function SumarNumeros (numUno, numDos){
+//     let resultado = numUno + numDos;
+//     return resultado;
+// }
+// resul = SumarNumeros (22, 10);
+// console.log(resul);
+
+// console.log(SumarNumeros (100, 10));
+
+// // Declarada sin parametros y con retorno
+// function SumarNumeros (){
+//     let numUno = 23;
+//     let numDos = 55;
+//     return numDos +numUno;
+
+// }
+// let suma = SumarNumeros ();
+// console.log(suma);
+
+// console.log(SumarNumeros ());

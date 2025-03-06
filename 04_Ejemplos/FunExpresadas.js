c7010fbdc57e4f394e3f9890ddcdc42f35f2e6a1
Pérez Niño Alvaro <155790737+aperezn298@users.noreply.github.com>
2025-03-06 13:29:53 -0500

    Funciones

---
+++ 04_Ejemplos/FunExpresadas.js
@@ -0,0 +1,15 @@
+// // Expresada sin parametros y sin retorno
+// const SumarNumeros = function (){
+//     let numUno = 23;
+//     let numDos = 10;
+//     let resultado = numDos + numUno;
+//     console.log(resultado);
+// }
+// SumarNumeros();
+
+// // Expresada con parametros y sin retorno
+// const SumarNumeros = function (uno, dos){
+//     let resultado = uno + dos;
+//     console.log(resultado);
+// }
+// SumarNumeros(34, 6);
